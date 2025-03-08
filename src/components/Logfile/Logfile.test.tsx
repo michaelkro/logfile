e2a7e6752077fb7d493a9b686ebb70228af037d8
Mike Ro <mikerodev@gmail.com>
2025-03-08 12:16:00 -0800

    feat: Create two column layout for log lines
    * Timestamp is displayed formatted as ISO 8601
    * Full event is displayed as single line JSON

---
+++ src/components/Logfile/Logfile.test.tsx
@@ -0,0 +1,2 @@
+test.todo('invalid JSON is reported to the user and does not block the UI')
+test.todo('renders log lines when request is successful')
--- src/components/Logfile/Logfile.tsx
+++ src/components/Logfile/Logfile.tsx
@@ -1,32 +1,94 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { useStreamLogFile } from '../../hooks/use-stream-log-file.tsx'
 
+import LogfileIcon from '../../../public/logfile.svg'
+
 import './Logfile.css'
+
+interface LogData {
+  [key: string]: string
+  _time: string
+}
+
+interface LogEntry {
+  timestamp?: string
+  raw: string
+  error?: string
+}
 
 export function Logfile() {
   const [url, setUrl] = useState<string>('')
+  const [logs, setLogs] = useState<LogEntry[]>([])
   const { streamLogfile, logLines, error } = useStreamLogFile()
+
+  useEffect(() => {
+    if (logLines && logLines.length > 0) {
+      const parsedLogs: LogEntry[] = logLines.map((logLine) => {
+        try {
+          const json: LogData = JSON.parse(logLine)
+          return { timestamp: new Date(json._time).toISOString(), raw: logLine }
+        } catch {
+          return { raw: logLine, error: 'Invalid JSON' }
+        }
+      })
+
+      setLogs(parsedLogs)
+    }
+  }, [logLines])
 
   return (
     <>
-      <header>Log Viewer</header>
+      <header className="logfile-header">
+        <img
+          className="logfile-header__brand"
+          src={LogfileIcon}
+          alt="logfile-svg"
+        />
+        <h1>logfile</h1>
+      </header>
       <main>
         <form
+          className="logfile-url-form"
           onSubmit={(e) => {
             e.preventDefault()
             streamLogfile(url)
           }}
         >
-          <input value={url} onChange={(e) => setUrl(e.target.value)} />
-          <button type="submit">Submit</button>
+          <div className="logfile-url-form__input">
+            <input
+              placeholder="Enter download URL for your log file"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+          </div>
+          {error && (
+            <span className="logfile-url-form__input__error">{error}</span>
+          )}
         </form>
-        {error}
-        <ul className="log-viewer-list">
-          {logLines.map((line, index) => (
-            <li key={index}>{line}</li>
-          ))}
-        </ul>
+        {logs.length > 0 && (
+          <div className="logfile-table">
+            <div className="logfile-table__header">
+              <div className="logfile-table__header__time-cell">Time</div>
+              <div className="logfile-table__header__event-cell">Event</div>
+            </div>
+            <div className="logfile-table__body">
+              {logs.map((log, index) => {
+                return (
+                  <div className="logfile-table__row" key={index}>
+                    <div className="logfile-table__row__time-cell">
+                      {log.timestamp}
+                    </div>
+                    <div className="logfile-table__row__event-cell">
+                      {log.raw}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
       </main>
     </>
   )
