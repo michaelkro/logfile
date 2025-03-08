5830ac873f86e6defe8157c7a1b9e943b9110d02
Mike Ro <mikerodev@gmail.com>
2025-03-08 10:29:39 -0800

    chore: Scaffold basic UI

--- src/components/Logfile/Logfile.tsx
+++ src/components/Logfile/Logfile.tsx
@@ -1,5 +1,33 @@
+import React, { useState } from 'react'
+
+import { useStreamLogFile } from '../../hooks/use-stream-log-file.tsx'
+
 import './Logfile.css'
 
 export function Logfile() {
-  return <div>hello</div>
+  const [url, setUrl] = useState<string>('')
+  const { streamLogfile, logLines, error } = useStreamLogFile()
+
+  return (
+    <>
+      <header>Log Viewer</header>
+      <main>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            streamLogfile(url)
+          }}
+        >
+          <input value={url} onChange={(e) => setUrl(e.target.value)} />
+          <button type="submit">Submit</button>
+        </form>
+        {error}
+        <ul className="log-viewer-list">
+          {logLines.map((line, index) => (
+            <li key={index}>{line}</li>
+          ))}
+        </ul>
+      </main>
+    </>
+  )
 }
