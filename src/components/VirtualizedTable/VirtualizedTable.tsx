5aaeb74909b32d1679870106fc2b491d0db314d0
Mike Ro <mikerodev@gmail.com>
2025-03-08 17:44:20 -0800

    chore: Derive necessary state for virtualization
    * We won't worry about expanding the rows yet

--- src/components/VirtualizedTable/VirtualizedTable.tsx
+++ src/components/VirtualizedTable/VirtualizedTable.tsx
@@ -1,16 +1,82 @@
-import { FC } from 'react'
+import { FC, useState, useRef, useEffect, useCallback } from 'react'
 
 import { LogEntry } from '../../types'
 
 import { ExpandableRow } from '../ExpandableRow/ExpandableRow'
 
 import './VirtualizedTable.css'
+import { COLLAPSED_ROW_HEIGHT, OVERSCAN_HEIGHT } from './constants'
+
+interface VirtualizedLogTableRow {
+  log: LogEntry
+  top: number
+  height: number
+}
 
 interface VirtualizedTableProps {
   logs: LogEntry[]
 }
 
 export const VirtualizedTable: FC<VirtualizedTableProps> = ({ logs }) => {
+  const [scrollTop, setScrollTop] = useState(0)
+  const [tableBodyHeight, setTableBodyHeight] = useState(0)
+  const tableBodyRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const tableBodyRefValue = tableBodyRef.current
+
+    if (tableBodyRefValue && logs.length > 0) {
+      setTableBodyHeight(tableBodyRefValue.clientHeight)
+
+      const resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          setTableBodyHeight(entry.contentRect.height)
+        }
+      })
+
+      resizeObserver.observe(tableBodyRefValue)
+
+      return () => {
+        if (tableBodyRefValue) {
+          resizeObserver.unobserve(tableBodyRefValue)
+        }
+      }
+    }
+  }, [logs])
+
+  const getRowHeight = useCallback((): number => {
+    return COLLAPSED_ROW_HEIGHT
+  }, [])
+
+  const getTotalHeight = useCallback((): number => {
+    return COLLAPSED_ROW_HEIGHT * logs.length
+  })
+
+  const getVirtualizedTableRows = useCallback((): VirtualizedLogTableRow[] => {
+    const virtualizedTableRows: VirtualizedLogTableRow[] = []
+    let currentTop = 0
+
+    logs.forEach((log) => {
+      const rowHeight = getRowHeight()
+      const rowBottom = currentTop + rowHeight
+
+      if (
+        rowBottom >= scrollTop - OVERSCAN_HEIGHT &&
+        currentTop <= scrollTop + tableBodyHeight + OVERSCAN_HEIGHT
+      ) {
+        virtualizedTableRows.push({
+          log,
+          top: currentTop,
+          height: rowHeight
+        })
+      }
+
+      currentTop += rowHeight
+    })
+
+    return virtualizedTableRows
+  }, [logs, scrollTop, tableBodyHeight, getRowHeight])
+
   return (
     logs.length > 0 && (
       <div className="logfile-table">
@@ -18,7 +84,11 @@ export const VirtualizedTable: FC<VirtualizedTableProps> = ({ logs }) => {
           <div className="logfile-table__header__time-cell">Time</div>
           <div className="logfile-table__header__event-cell">Event</div>
         </div>
-        <div className="logfile-table__body">
+        <div
+          className="logfile-table__body"
+          onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}
+          ref={tableBodyRef}
+        >
           {logs.map((log, index) => {
             return <ExpandableRow log={log} index={index} />
           })}
