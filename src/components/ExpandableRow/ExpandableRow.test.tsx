099c4b9fc595b8733f94432683742d578dbcc875
Michael Ro <mikerodev@gmail.com>
2025-03-09 20:39:15 -0700

    test: Add tests and todos (#16)

--- src/components/ExpandableRow/ExpandableRow.test.tsx
+++ src/components/ExpandableRow/ExpandableRow.test.tsx
@@ -1,2 +1,47 @@
+import { render, screen } from '@testing-library/react'
+
+import { LogEntry } from '../../types'
+
+import { ExpandableRow } from './ExpandableRow'
+
+const toggleExpand = jest.fn()
+const setHeightFromRef = jest.fn()
+
+test('renders collapsed row correctly', () => {
+  const timestamp = 'foo'
+  const raw = 'rawr'
+  const log: LogEntry = {
+    id: 1,
+    timestamp,
+    raw
+  }
+  const top = 100
+  const height = 50
+
+  render(
+    <ExpandableRow
+      log={log}
+      top={top}
+      height={height}
+      expanded={false}
+      toggleExpand={toggleExpand}
+      setHeightFromRef={setHeightFromRef}
+    />
+  )
+
+  expect(screen.getByText(timestamp)).toBeInTheDocument()
+  expect(screen.getByText(raw)).toBeInTheDocument()
+
+  const row = screen.getByRole('row')
+
+  expect(row).toHaveStyle({
+    transform: `translateY(${top}px)`,
+    height: `${height}px`
+  })
+
+  expect(row).not.toHaveClass('expanded')
+})
+
+test.todo('renders expanded row correctly')
 test.todo('calls setHeightFromRef when expanded')
 test.todo('calls toggleExpand with correct arguments on click')
